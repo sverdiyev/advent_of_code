faef264d3fa1acb1dcbafc66d087e1d413db03e4
sverdiyev <79407224+sverdiyev@users.noreply.github.com>
2023-12-03 17:54:14 +0100

    solve 2-2

--- 2023/src/test/java/org/sverdiye/TaskTwoTest.java
+++ 2023/src/test/java/org/sverdiye/TaskTwoTest.java
@@ -8,6 +8,7 @@ import java.util.Map;
 public class TaskTwoTest {
 
     private Map<Integer, Boolean> resultsMap = new HashMap<>();
+    private Map<Integer, Integer> powersMap = new HashMap<>();
 
     @Test
     public void test() {
@@ -18,6 +19,7 @@ public class TaskTwoTest {
         }
 
         System.out.println(parseResultMap());
+        System.out.println(parsePowerMap());
     }
 
     private void parseLine(String line) {
@@ -29,10 +31,10 @@ public class TaskTwoTest {
         String[] gameSets = gameResult.split("; ");
 
         final int maxRed = 12, maxGreen = 13, maxBlue = 14;
+        int pulledRed = 0, pulledGreen = 0, pulledBlue = 0;
 
         boolean isGamePossible = true;
         for (String set : gameSets) {
-            int pulledRed = 0, pulledGreen = 0, pulledBlue = 0;
 
             var ballPulls = set.split(", ");
 
@@ -43,16 +45,22 @@ public class TaskTwoTest {
 
                 switch (color) {
                     case "blue" -> {
+                        pulledBlue = Math.max(pulledBlue, number);
+
                         if ((number > maxBlue)) {
                             isGamePossible = false;
                         }
                     }
                     case "green" -> {
+                        pulledGreen = Math.max(pulledGreen, number);
+
                         if ((number > maxGreen)) {
                             isGamePossible = false;
                         }
                     }
                     case "red" -> {
+                        pulledRed = Math.max(pulledRed, number);
+
                         if ((number > maxRed)) {
                             isGamePossible = false;
                         }
@@ -62,9 +70,14 @@ public class TaskTwoTest {
             }
         }
         resultsMap.put(gameId, isGamePossible);
+        powersMap.put(gameId, pulledRed * pulledBlue * pulledGreen);
     }
 
     private Integer parseResultMap() {
         return resultsMap.entrySet().stream().filter(Map.Entry::getValue).mapToInt(Map.Entry::getKey).sum();
+    }
+
+    private Integer parsePowerMap() {
+        return powersMap.entrySet().stream().mapToInt(Map.Entry::getValue).sum();
     }
 }
