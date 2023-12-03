7adbd26555f379a385fd1796ce3c9ee0fa6da767
sverdiyev <79407224+sverdiyev@users.noreply.github.com>
2023-12-03 12:57:48 +0100

    add files reader

--- 2023/src/test/java/org/sverdiye/TaskOne.java
+++ 2023/src/test/java/org/sverdiye/TaskOne.java
@@ -1,6 +1,15 @@
 package org.sverdiye;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 public class TaskOne {
 
-//    @Test
+    @Test
+    public void partOne() {
+        List<String> lines = TestUtils.readFile(1);
+        System.out.println(lines);
+
+    }
 }
---
+++ 2023/src/test/java/org/sverdiye/TestUtils.java
@@ -0,0 +1,23 @@
+package org.sverdiye;
+
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class TestUtils {
+
+    public static List<String> readFile(int nOfTask) {
+        String filePath = "data/%s.txt".formatted(nOfTask);
+        try {
+            URL resourceUri = TestUtils.class.getClassLoader().getResource(filePath);
+            Stream<String> lines = Files.lines(Path.of(resourceUri.toURI()));
+
+            return lines.toList();
+        } catch (Exception e) {
+            System.err.println("Error while reading file");
+            return null;
+        }
+    }
+}
