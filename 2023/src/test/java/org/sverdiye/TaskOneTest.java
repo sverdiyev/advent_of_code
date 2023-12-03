f43e9cbe115690b3b27f4001c691340ca249e90a
sverdiyev <79407224+sverdiyev@users.noreply.github.com>
2023-12-03 14:45:56 +0100

    complete task 1-2

--- 2023/src/test/java/org/sverdiye/TaskOneTest.java
+++ 2023/src/test/java/org/sverdiye/TaskOneTest.java
@@ -12,18 +12,38 @@ public class TaskOneTest {
         List<String> lines = TestUtils.readFile(1);
 
 
-        List<List<Character>> linesWithOutLetters = new ArrayList<>();
+        List<List<Integer>> linesWithOutLetters = new ArrayList<>();
         Integer sum = 0;
         for (String line : lines) {
-            ArrayList<Character> newList = new ArrayList<>();
+            ArrayList<Integer> newList = new ArrayList<>();
             linesWithOutLetters.add(newList);
-            for (char ch : line.toCharArray()) {
-                if (Character.isDigit(ch)) {
-                    newList.add(ch);
+            for (int i = 0; i < line.length(); i++) {
+
+                char myChar = line.charAt(i);
+                if (Character.isDigit(myChar)) {
+                    newList.add(Character.getNumericValue(myChar));
+                } else {
+                    int mappedDigit = isWordedDigit(line, i);
+                    if (mappedDigit > -1) newList.add(mappedDigit);
                 }
             }
             sum += Integer.parseInt("" + newList.getFirst() + newList.getLast());
         }
         System.out.println(sum);
+    }
+
+    private int isWordedDigit(String line, int index) {
+        List<String> wordedDigits = List.of("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine");
+
+        int min = wordedDigits.stream().reduce((s1, s2) -> s1.length() < s2.length() ? s1 : s2).get().length();
+        if (line.length() < min)
+            return -1;
+
+        for (int i = 0; i < wordedDigits.size(); i++) {
+            if (line.startsWith(wordedDigits.get(i), index)) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
